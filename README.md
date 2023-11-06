04baa74e8f1ac6dff331050602bd6acf54276d5b
Eddy Gomet <eddygomet@gmail.com>
2023-11-06 18:35:56 +0100

    .feat grande suite et full + README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Testing
+
+Pour installer les dépendances (jest en l'occurence): 
+"npm i"
+
+Pour effectuer un test 
+"npm run test"
--- index.js
+++ index.js
@@ -4,26 +4,50 @@ const yams = (dice1, dice2, dice3, dice4, dice5)=>{
     let arrayDice = [dice1, dice2, dice3, dice4, dice5];
     arrayDice.sort();
 
-    //pour le brelan
-    for (let i = 0; i < arrayDice.length - 2; i++) {
-        if (arrayDice[i] === arrayDice[i + 1] && arrayDice[i] === arrayDice[i + 2]) {
+    //Pour le BRELAN
+    for (let index = 0; index < arrayDice.length - 2; index++) {
+        if (arrayDice[index] === arrayDice[index + 1] && arrayDice[index] === arrayDice[index + 2]) {
             resultDice = 28;
         }
     }
 
-    //pour le carré
-    for (let i = 0; i < arrayDice.length - 3; i++) {
-        if (arrayDice[i] === arrayDice[i + 1] && arrayDice[i] === arrayDice[i + 2] && arrayDice[i] === arrayDice[i + 3]) {
+    //pour le CARRE
+    for (let index = 0; index < arrayDice.length - 3; index++) {
+        if (arrayDice[index] === arrayDice[index + 1] && arrayDice[index] === arrayDice[index + 2] && arrayDice[index] === arrayDice[index + 3]) {
             resultDice = 35;
         }
     }
 
-    //pour le yams
-    for (let i = 0; i < arrayDice.length - 4; i++) {
-        if (arrayDice[i] === arrayDice[i + 1] && arrayDice[i] === arrayDice[i + 2] && arrayDice[i] === arrayDice[i + 3] && arrayDice[i] === arrayDice[i + 4]) {
+    //pour le YAMS
+    for (let index = 0; index < arrayDice.length - 4; index++) {
+        if (arrayDice[index] === arrayDice[index + 1] && arrayDice[index] === arrayDice[index + 2] && arrayDice[index] === arrayDice[index + 3] && arrayDice[index] === arrayDice[index + 4]) {
             resultDice = 50;
         }
     }
+
+    //Pour faire un FULL
+    for (let index = 0; index < arrayDice.length - 2; index++) {
+        if (arrayDice[index] === arrayDice[index + 1] && arrayDice[index] === arrayDice[index + 2]) {
+            // Si un brelan est trouvé, vérifiez s'il y a une paire
+            for (let j = 0; j < arrayDice.length - 1; j++) {
+                if (arrayDice[j] === arrayDice[j + 1] && arrayDice[j] !== arrayDice[index]) {
+                    resultDice = 30;
+                }
+            }
+        }
+    }
+
+    //Pour la GRANDE SUITE
+    for(let index = 0; index < arrayDice.length; index++){
+        if(arrayDice[index] < arrayDice[index + 1]
+            && arrayDice[index + 1] < arrayDice[index + 2]
+            && arrayDice[index + 2] < arrayDice[index + 3]
+            && arrayDice[index + 3] < arrayDice[index + 4] )
+        {
+            resultDice = 40;
+        }
+    }
+
 
     return resultDice;
 }
